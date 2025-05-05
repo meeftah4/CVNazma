3ef0ac7777578eebc38f40187a035f9822cf2f86
sherlymarsa <marsarenaaa@gmail.com>
2025-05-05 08:26:36 +0700

    profile

--- resources/views/pages/profile.blade.php
+++ resources/views/pages/profile.blade.php
@@ -29,3 +29,15 @@
     </div>
 </div>
 @endsection
+
+<div class="profile-card">
+    <div class="profile-bg"></div>
+    <div class="profile-avatar">
+        <img src="avatar.jpg" alt="Avatar">
+    </div>
+    <h2>Nama Pengguna</h2>
+    <ul>
+        <li><a href="/profile" class="active">Profil</a></li>
+        <li><a href="/cv-ats">CV ATS Saya</a></li>
+    </ul>
+</div>
